cdfc027b5b208f9600d96b95c44e46e67d216db1
Jacopo Scazzosi <jacopo@scazzosi.com>
2022-11-06 21:16:28 +0100

    v0.2.1

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 
 export type { LoggerOpts, LogWriter } from './types';
 
+export { NodeBufferedWriter } from './writers/node-buffered';
+export { ConsoleWriter } from './writers/console';
+
 import { createLogger } from './logger';
 
 export { createLogger };
