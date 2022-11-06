b6d780afdf62bef5a565a5ef002be39a8287f443
Jacopo Scazzosi <jacopo@scazzosi.com>
2022-11-06 21:37:33 +0100

    v0.2.2

--- src/constants.ts
+++ src/constants.ts
@@ -6,3 +6,5 @@ export const LEVELS: LogLevel[] = ['trace', 'debug', 'info', 'warn', 'error'];
 export const NOOP = () => {};
 
 export const EMPTY = Object.create(null);
+
+export const RESOLVED = Promise.resolve();
--- src/writers/node-buffered.ts
+++ src/writers/node-buffered.ts
@@ -1,7 +1,7 @@
 
 import type { LogArg, LogWriter, LogLevel } from '../types';
 import { format } from '../format';
-import { EMPTY } from '../constants';
+import { EMPTY, RESOLVED } from '../constants';
 
 export interface NodeBufferedWriterOpts {
   maxBufferLength?: number;
@@ -12,12 +12,14 @@ export class NodeBufferedWriter implements LogWriter {
 
   private _buffer: string;
   private _timeout: any;
+  private _flushing: boolean;
   private readonly _maxBufferLength: number;
   private readonly _flushTimeout: number;
 
   constructor(opts: NodeBufferedWriterOpts = EMPTY) {
     this._buffer = '';
     this._timeout = null;
+    this._flushing = false;
     this._maxBufferLength = opts?.maxBufferLength || 8192;
     this._flushTimeout = opts?.flushTimeout || 250;
     this._flush = this._flush.bind(this);
@@ -30,14 +32,19 @@ export class NodeBufferedWriter implements LogWriter {
       clearTimeout(this._timeout);
       this._timeout = null;
     }
+    this._flushing = false;
   }
 
   write(level: LogLevel, prefix: string, message: string, args: LogArg[]): any {
     this._buffer += format(level, prefix, message, args) + '\n';
-    if (this._buffer.length >= this._maxBufferLength) {
-      this._flush();
-    } else if (this._timeout === null) {
-      this._timeout = setTimeout(this._flush, this._flushTimeout);
+    if (!this._flushing) {
+      if (this._buffer.length >= this._maxBufferLength) {
+        this._flushing = true;
+        RESOLVED.then(this._flush);
+      } else if (this._timeout === null) {
+        this._flushing = true;
+        this._timeout = setTimeout(this._flush, this._flushTimeout);
+      }
     }
   }
 }
