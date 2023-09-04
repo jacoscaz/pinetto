b7888aae4ac0ee758ed7ad89c22877444bedd061
Jacopo Scazzosi <jacopo@scazzosi.com>
2023-09-04 12:27:10 +0200

    v1.3.2, renames NodeBufferedWriter to BufferedWriter in exports

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 
 export type { LoggerOpts, LogWriter, LogLevel, LogArg } from './types.js';
 
-export { BufferedWriter as NodeBufferedWriter } from './writers/node-buffered.js';
+export { BufferedWriter } from './writers/buffered.js';
 export { ConsoleWriter } from './writers/console.js';
 
 import { createLogger, Logger } from './logger.js';
--- src/writers/default.ts
+++ src/writers/default.ts
@@ -1,7 +1,7 @@
 
 import type { LogWriter } from '../types.js';
 import { IS_NODE } from '../constants.js';
-import { BufferedWriter } from './node-buffered.js';
+import { BufferedWriter } from './buffered.js';
 import { ConsoleWriter } from './console.js';
 
 export const DefaultWriter: new () => LogWriter = IS_NODE ? BufferedWriter : ConsoleWriter;
