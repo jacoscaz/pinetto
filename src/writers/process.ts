0a50463fb3f71df31fc8945fad6042b7208dfa7c
Jacopo Scazzosi <jacopo@scazzosi.com>
2025-01-18 12:26:50 +0100

    configurable stream in ProcessWriter

--- src/writers/process.ts
+++ src/writers/process.ts
@@ -3,17 +3,35 @@ import type { LogWriter } from '../types.js';
 
 import { EOL, IS_NODE } from '../constants.js';
 
+interface WriteStream {
+  write(data: string): any;
+}
+
+const getStream = (stream: 'stdout' | 'stderr' | WriteStream): WriteStream => {
+  switch (stream) {
+    case 'stdout':
+      return process.stdout;
+    case 'stderr':
+      return process.stderr;
+    default:
+      return stream;
+  }
+};
+
 export class ProcessWriter implements LogWriter {
 
-  constructor() {
+  #stream: WriteStream;
+
+  constructor(stream: 'stdout' | 'stderr' | WriteStream = 'stdout') {
     if (!IS_NODE) {
       throw new Error('Cannot use ProcessWriter(): Node.js not detected');
     }
+    this.#stream = getStream(stream);
   }
 
   write(entry: string): any {
-    process.stdout.write(entry);
-    process.stdout.write(EOL.value);
+    this.#stream.write(entry);
+    this.#stream.write(EOL.value);
   }
 
 }
